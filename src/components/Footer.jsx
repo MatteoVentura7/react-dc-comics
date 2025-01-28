c6407372fec87df065d7c4758a499426c39f87e9
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-28 18:11:04 +0100

    add : footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,131 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-top">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum
-        expedita nobis, mollitia error quidem architecto ratione sit molestias
-        tenetur quo? Nostrum doloribus corrupti consequuntur sapiente voluptatum
-        quasi, excepturi illum!
+      <div className="back-img">
+        <div className="container footer-top row">
+          <div className="col">
+            <h3>DC COMICS</h3>
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col">
+            <h3>DC</h3>
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col">
+            <h3>SITES</h3>
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col">
+            <h3>SHOP</h3>
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <img src="public\img\dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <div className="container fot">
+          <div className="footer-left">
+            <button className="btn">SIGN-UP NOW!</button>
+          </div>
+          <div className="footer-right">
+            <nav className="main-nav">
+              <ul id="nav-list">
+                <li>
+                  <h2>FOLLOW US!</h2>
+                </li>
+                <li>
+                  <img src="public\img\footer-facebook.png" alt="" />
+                </li>
+                <li>
+                  <img src="public\img\footer-twitter.png" alt="" />
+                </li>
+                <li>
+                  <img src="public\img\footer-youtube.png" alt="" />
+                </li>
+                <li>
+                  <img src="public\img\footer-pinterest.png" alt="" />
+                </li>
+                <li>
+                  <img src="public\img\footer-periscope.png" alt="" />
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </footer>
   );
