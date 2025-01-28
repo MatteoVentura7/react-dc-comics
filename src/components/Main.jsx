faf347e1fcd3f0b2b721915b54b6cf429ac6a7c4
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-28 18:39:15 +0100

    add : fix details

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,36 +8,55 @@ export default function Main() {
       </div>
       <div className="rowTwo">
         <div className="container service">
-          <div>
-            <img
-              className="img-size"
-              src="\img\buy-comics-digital-comics.png"
-              alt=""
-            />
+          <div className="details">
+            <div>
+              <img
+                className="img-size"
+                src="\img\buy-comics-digital-comics.png"
+                alt=""
+              />
+            </div>
+            <div className="img-details">DIGITAL COMICS</div>
           </div>
-          <div>
-            <img
-              className="img-size"
-              src="\img\buy-comics-merchandise.png"
-              alt=""
-            />
+          <div className="details">
+            <div>
+              <img
+                className="img-size"
+                src="\img\buy-comics-merchandise.png"
+                alt=""
+              />
+            </div>
+            <div className="img-details">DC MERCHANDISE</div>
           </div>
-          <div>
-            <img
-              className="img-size"
-              src="\img\buy-comics-shop-locator.png"
-              alt=""
-            />
+          <div className="details">
+            <div>
+              <img
+                className="img-size"
+                src="\img\buy-comics-shop-locator.png"
+                alt=""
+              />
+            </div>
+            <div className="img-details">SUBSCRIPTION</div>
           </div>
-          <div>
-            <img
-              className="img-size"
-              src="\img\buy-comics-subscriptions.png"
-              alt=""
-            />
+          <div className="details">
+            <div>
+              <img
+                className="img-size"
+                src="\img\buy-comics-subscriptions.png"
+                alt=""
+              />
+            </div>
+            <div className="img-details">COMIC SHOP LOCATOR</div>
           </div>
-          <div>
-            <img className="img-size" src="\img\buy-dc-power-visa.svg" alt="" />
+          <div className="details">
+            <div>
+              <img
+                className="img-size"
+                src="\img\buy-dc-power-visa.svg"
+                alt=""
+              />
+            </div>
+            <div className="img-details">DC POWER VISA</div>
           </div>
         </div>
       </div>
