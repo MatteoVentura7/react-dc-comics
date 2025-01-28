87e4a69e0f7491b5eadc99b979362f74e0c9acbc
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-28 17:03:36 +0100

    add : section two

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <div className="footer-top">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex rerum
+        expedita nobis, mollitia error quidem architecto ratione sit molestias
+        tenetur quo? Nostrum doloribus corrupti consequuntur sapiente voluptatum
+        quasi, excepturi illum!
+      </div>
+    </footer>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,6 +6,41 @@ export default function Main() {
           <h1>--&gt; Content goes here &lt;--</h1>
         </div>
       </div>
+      <div className="rowTwo">
+        <div className="container service">
+          <div>
+            <img
+              className="img-size"
+              src="\img\buy-comics-digital-comics.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-size"
+              src="\img\buy-comics-merchandise.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-size"
+              src="\img\buy-comics-shop-locator.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              className="img-size"
+              src="\img\buy-comics-subscriptions.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img className="img-size" src="\img\buy-dc-power-visa.svg" alt="" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
