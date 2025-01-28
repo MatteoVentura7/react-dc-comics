1e00b4367abc0040a9a77fd0c2937f46c51c77f4
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-28 16:11:56 +0100

    add : header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,65 @@
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header>
+      <div>
+        <img src="/img/dc-logo.png" alt="" />
+      </div>
+      <div className="header-right">
+        <nav className="main-nav">
+          <ul id="nav-list">
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>CHARACTERS</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>COMICS</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>MOVIES</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>TV</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>GAMES</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>COLLECTIBLES</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>VIDEOS</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>FANS</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>NEWS</strong>
+              </a>
+            </li>
+            <li>
+              <a className="animated-underline" href="#">
+                <strong>SHOP</strong>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
