25658f44075ad8b0800b0cd99f83380f0978ced5
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-29 15:36:02 +0100

    add : header prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,64 +1,79 @@
+const links = [
+  {
+    id: 1,
+    text: "CHARACTERS",
+    url: "#",
+  },
+  {
+    id: 2,
+    text: "COMICS",
+    url: "#",
+    current: true,
+  },
+  {
+    id: 3,
+    text: "MOVIES",
+    url: "#",
+  },
+  {
+    id: 4,
+    text: "TV",
+    url: "#",
+  },
+  {
+    id: 5,
+    text: "GAMES",
+    url: "#",
+  },
+  {
+    id: 6,
+    text: "COLLECTIBLES",
+    url: "#",
+  },
+  {
+    id: 7,
+    text: "VIDEOS",
+    url: "#",
+  },
+  {
+    id: 8,
+    text: "FANS",
+    url: "#",
+  },
+  {
+    id: 9,
+    text: "NEWS",
+    url: "#",
+  },
+  {
+    id: 10,
+    text: "SHOP",
+    url: "#",
+  },
+];
+
 export default function Header() {
   return (
-    <header className="container">
-      <div className="logo">
-        <img src="/img/dc-logo.png" alt="" />
-      </div>
-      <div className="header-right">
-        <nav className="main-nav">
-          <ul id="nav-list">
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>CHARACTERS</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>COMICS</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>MOVIES</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>TV</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>GAMES</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>COLLECTIBLES</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>VIDEOS</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>FANS</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>NEWS</strong>
-              </a>
-            </li>
-            <li>
-              <a className="animated-underline" href="#">
-                <strong>SHOP</strong>
-              </a>
-            </li>
-          </ul>
-        </nav>
+    <header>
+      <div className="container">
+        <div className="logo">
+          <img src="/img/dc-logo.png" alt="" />
+        </div>
+        <div className="header-right">
+          <nav className="main-nav">
+            <ul id="nav-list">
+              {links.map((link) => {
+                return (
+                  <li key={link.id}>
+                    <a className="animated-underline" href={link.url}>
+                      {link.text}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
