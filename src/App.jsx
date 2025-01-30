212a15b76264857ec8f94ed872da2c46d712f52f
MatteoVentura <matteo.ventura18@gmail.com>
2025-01-30 13:16:16 +0100

    add : hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Main />
       <Footer />
     </>
